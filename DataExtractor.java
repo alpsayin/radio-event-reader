6d1def5844d3b96e5e45136e25ab2452241f54fa
Alp Sayin <alpsayin@gmail.com>
2012-09-20 17:21:02 +0300

    adding a data extractor for separating 127 byte and 2kbyte transfers

---
+++ DataExtractor.java
@@ -0,0 +1,54 @@
+
+import java.io.*;
+import java.util.*;
+
+public class DataExtractor
+{
+	public static void main(String args[]) throws Exception
+	{
+		if(args.length < 1)
+		{
+			System.out.println("Where is the input file?");
+			System.exit(1);
+		}
+
+		String inputFilename = args[0];
+		File inputFile = new File(inputFilename);
+		String outputFilename = inputFile.getName();
+		if(args.length > 1)
+		{
+			outputFilename = args[1];
+		}
+
+		File outputFile127 = new File(outputFilename+".127");
+		File outputFile2k= new File(outputFilename+".2k");
+
+		Scanner scan = new Scanner(inputFile);
+		PrintWriter pw127 = new PrintWriter(outputFile127);
+		PrintWriter pw2k = new PrintWriter(outputFile2k);
+		PrintWriter currentWriter = null;
+
+		String line;
+		while(scan.hasNextLine())
+		{
+			line = scan.nextLine();
+			if(line.contains("[put->"))
+			{
+				if(line.contains("text127"))
+				{
+					currentWriter = pw127;
+				}
+				if(line.contains("text2k"))
+				{
+					currentWriter = pw2k;
+				}
+			}
+			currentWriter.println(line);
+			currentWriter.flush();
+		}
+
+		scan.close();
+		pw127.close();
+		pw2k.close();
+	}
+}
