0f9862d426fcb702d662363d633dd6f597f1ab1e
Alp Sayin <alpsayin@gmail.com>
2012-09-28 18:24:50 +0300

    added initialization and save commands to reader scripts

--- readerScriptBim2a.m
+++ readerScriptBim2a.m
@@ -1,4 +1,7 @@
-
+close all
+clear all
+javaclasspath('.')
 for location=[0 1 2 4]
     [bim2a{location+1} bim2a_average127{location+1} bim2a_average2k{location+1}] = readEventLogs(['/Users/alpsayin/Documents/MATLAB/data/8Jul2012_GamlaStan_434MhzExperiments/radiotftp/bim2a/MobileStation/radiotftp_uhf-event.log.' 48+location])
 end
+save('bim2a.mat')
--- readerScriptUhx1.m
+++ readerScriptUhx1.m
@@ -1,3 +1,7 @@
+close all
+clear all
+javaclasspath('.')
 for location=0:7
     [uhx1{location+1} uhx1_average127{location+1} uhx1_average2k{location+1}] = readEventLogs(['/Users/alpsayin/Documents/MATLAB/data/7Jul2012_GamlaStan_144MhzExperiments/radiotftp/uhx1/MobileStation/radiotftp_vhf-event.log.' 48+location])
 end
+save('uhx1.mat')
