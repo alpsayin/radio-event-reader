8364238f52606d60bb0e28541cc914be34d6ec2e
Alp Sayin <alpsayin@gmail.com>
2012-09-30 00:12:46 +0300

    discarding the disconnected cases

--- plotterScript.m
+++ plotterScript.m
@@ -4,11 +4,13 @@ variables = {uhx1_average127, uhx1_average2k, bim2a_average127, bim2a_average2k}
 variable_names = {'Uhx1 127 Bytes', 'Uhx1 2 KBytes', 'Bim2a 127 Bytes', 'Bim2a 2 KBytes'};
 for variable_index = 1:length(variables)
 
+    xdata = {};
+    ydata = {};
+    
     data_struct = variables{variable_index};
     data_name = variable_names{variable_index};
     
     distances = [1 ; 395 ; 700 ; 1050 ; 1390 ; 1820 ; 1950 ; 2120];
-    ydata = zeros(length(distances), length(data_struct));
     
     plot_list = {   ...
                     'transferTime', ...
@@ -35,25 +37,33 @@ for variable_index = 1:length(variables)
                    'energy', ...
                    'throughput' ...
                    };
-
+    
     for ii=1:length(plot_list)
         fieldname = plot_list{ii};
+        index = 1;
         for jj=1:length(distances)
             if  jj<=length(data_struct) && ~isempty(data_struct{jj})
-                value = GETFIELD(data_struct{jj}, fieldname);
+                value = getfield(data_struct{jj}, fieldname);
                 if isnan(value)
-                    value = -max(ydata(:,ii));
+                    value = -max(ydata{ii});
                 end
             else
-                value = -max(ydata(:,ii));
+                value = -max(ydata{ii});
             end
-            ydata(jj, ii) = value;
+            if value>= 0
+                ydata{ ii}(index) = value;
+                xdata{ ii}(index) = distances(index);
+                index = index + 1;
+            end
         end
     end
 
     for ii=1:length(plot_list)
         figure
-        stem(distances, ydata(:,ii))
+%         bar(xdata{ii}, ydata{ii})
+        hold on
+        stem(xdata{ii}, ydata{ii}, 'r')
+        
         title(data_name)
         xlabel('Distance (meters)')
         ylabel(plot_labels{ii})
